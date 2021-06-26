e23e74fb585ac97a6521f271a64c0155e35ae5ee
Orlando Silva <orlando.silva.contato@gmail.com>
2021-06-25 22:54:52 -0300

    Adding express listener and application files

---
+++ src/Server.ts
@@ -0,0 +1,10 @@
+const app = require('./app')
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
+const PORT: number = parseInt(process.env.PORT as string, 10) || 7000
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
