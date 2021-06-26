53afbcb17552202122d284f36f2a8397b3c57a8a
Orlando Silva <orlando.silva.contato@gmail.com>
2021-06-25 23:46:55 -0300

    Adding an endpoint to 'Get balance for non-existing account'

--- src/application/controllers/EventController.ts
+++ src/application/controllers/EventController.ts
@@ -1,4 +1,4 @@
-import { Response, Router } from 'express'
+import { Request, Response, Router } from 'express'
 import { injectable } from 'tsyringe'
 import AccountService from '../../domain/services/AccountService'
 
@@ -15,9 +15,22 @@ export default class EventController {
         this.accountService.reset()
         res.status(200).send('OK')
     }
+
+    getBalanceById(req: Request, res: Response) {
+        try {
+            return res.status(200).json (
+                this.accountService.getBalanceById(parseInt(req.query.account_id!.toString()))
+            )
+        } catch(e) {
+            res.status(404).json(0)
+        }
+    }
   
     routes() {
         this.router.post('/reset', (_req, res) => res.send(this.reset(res)))
+        this.router.get('/balance', (_req, res) => res.send(this.getBalanceById(_req, res)))
         return this.router
     }
+
+    
 }
