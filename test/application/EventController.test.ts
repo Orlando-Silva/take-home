b752d487c16290ad96f9f965b6c4ddb88c88022a
Orlando Silva <orlando.silva.contato@gmail.com>
2021-06-25 23:48:43 -0300

    Adding a test for 'Get balance for non-existing account' endpoint

--- test/application/EventController.test.ts
+++ test/application/EventController.test.ts
@@ -1,13 +1,20 @@
 
 const request = require('supertest')
-const app = require("../../src/App")
+const app = require('../../src/App')
 
-describe("Event Controller Tests", () => {
+describe('Event Controller Tests', () => {
 
-    test("Reset state before starting tests", () => {
+    test('Reset state before starting tests', () => {
       return request(app)
-        .post("/reset")
+        .post('/reset')
         .expect(200, 'OK');
     })
+
+    test('Get balance for non-existing account', () => {
+        return request(app)
+          .get('/balance')
+          .query({ account_id: '1234' })
+          .expect(404, '0');
+    });  
 
 })
