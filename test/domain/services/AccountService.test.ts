955cce12dfae540f5250bc992bc9d6063dfa2163
Orlando Silva <orlando.silva.contato@gmail.com>
2021-06-26 00:25:14 -0300

    Adding test for 'Deposit into existing account' and small refactor in tests structure

--- test/domain/services/AccountService.test.ts
+++ test/domain/services/AccountService.test.ts
@@ -4,28 +4,53 @@ import Account from '../../../src/domain/models/Account';
 import NonExistingAccountException from '../../../src/domain/models/exceptions/NonExistingAccountException';
 import AccountService from '../../../src/domain/services/AccountService'
 
-describe('Account Service Tests', () => {
+let accountService = container.resolve(AccountService)
 
-    let accountService = container.resolve(AccountService)
-
-    test('Reset state before starting tests', () => {
-        expect(accountService.reset()).toEqual([])  
-    })
+describe('Get Balance Tests', () => {
 
     test('Get balance for non-existing account', () => {
         expect(() => accountService.getBalanceById(1234)).toThrowError(NonExistingAccountException)
     })
 
+})
+
+describe('Deposits Tests', () => {
+
+    beforeEach(() => {
+        container.clearInstances() // Flush singleton instances to make sure repository won't carry anything from other tests.
+        accountService = container.resolve(AccountService)
+    })
+
     test('Create account with initial balance', () => {
-
-        const expectedResult: Account = {
-            id: 100,
-            balance: 10
-        }
-
+        const expectedResult = createMockAccount(100, 10)
         const actualResult = accountService.createTransaction(100, 10)
 
         expect(actualResult).toStrictEqual(expectedResult)
     })
 
+
+    test('Deposit into existing account', () => {
+        const expectedResult = createMockAccount(100, 20)
+
+        accountService.createTransaction(100, 10)
+        const actualResult = accountService.createTransaction(100, 10)
+
+        expect(actualResult).toStrictEqual(expectedResult)
+    })
 })
+
+describe('Reset State Tests', () => {
+
+    test('Reset state before starting tests', () => {
+        expect(accountService.reset()).toEqual([])  
+    })
+
+})
+
+
+function createMockAccount(id: number, balance: number) : Account  {
+    return { 
+        id: id,
+        balance: balance
+    }
+}
